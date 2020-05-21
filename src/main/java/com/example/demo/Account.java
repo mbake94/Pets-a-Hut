d84efb2c49520d7ead49a037daa8b10b0079ae06
mbake94 <mbake94@gmail.com>
2020-05-21 11:06:06 -0700

    added all html pages, and nav bar. needs content and css

---
+++ src/main/java/com/example/demo/Account.java
@@ -0,0 +1,93 @@
+package com.example.demo;
+
+public class Account {
+    private String personName;
+    private String phone;
+    private String petInfo;
+    private String petName;
+    private int age;
+    private String gender;
+    private String petType;
+    private String breed;
+
+    public Account(String personName, String phone, String petInfo, String petName, int age, String gender, String petType, String breed) {
+        this.personName = personName;
+        this.phone = phone;
+        this.petInfo = petInfo;
+        this.petName = petName;
+        this.age = age;
+        this.gender = gender;
+        this.petType = petType;
+        this.breed = breed;
+    }
+
+    public Account() {
+
+    }
+
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPetInfo() {
+        return petInfo;
+    }
+
+    public void setPetInfo(String petInfo) {
+        this.petInfo = petInfo;
+    }
+
+    public String getPetName() {
+        return petName;
+    }
+
+    public void setPetName(String petName) {
+        this.petName = petName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPetType() {
+        return petType;
+    }
+
+    public void setPetType(String petType) {
+        this.petType = petType;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+}
+
--- src/main/java/com/example/demo/StoreController.java
+++ src/main/java/com/example/demo/StoreController.java
@@ -1,4 +1,26 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
 public class StoreController {
+    @GetMapping(value = {"/"})
+    public String loadStore(Model model){
+        model.addAttribute("account", new Account());
+
+        return "login";
+    }
+    @PostMapping("/Account")
+    public String loadStore(@Valid Account account, Model model){
+
+        model.addAttribute("account", account);
+
+        return "home";
+    }
+
 }
