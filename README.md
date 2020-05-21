b7cefa45d097444eb2179e72c0a1fd88a0b2ee9b
mbake94 <mbake94@gmail.com>
2020-05-21 07:26:58 -0700

    created login page

---
+++ README.md
@@ -0,0 +1,1 @@
+# Pets-a-Hut
---
+++ src/main/java/com/example/demo/PetsaHutApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsaHutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetsaHutApplication.class, args);
+    }
+
+}
